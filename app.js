984a04c150e3bca26ff7e2e82b151b1e08ba54b8
pauldidonatojr <pjdidonatojr@gmail.com>
2022-06-18 17:21:39 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+ .base('app1LVKkET0KmS2gL')
+ .table('restaurants')
+
+exports.handler = async function handler(event, context) {
+ try {
+  const { records } = await airtable.list()
+  const products = records.map((product) => {
+   const { id } = product
+   const { name, category, image, location } = product.fields
+   const url = image[0].url
+   console.log(id, name, url, category, location)
+   return { id, name, url, category, location }
+  })
+  return {
+   headers: {
+    'Access-Control-Allow-Origin': '*',
+   },
+   statusCode: 200,
+   body: JSON.stringify(products),
+  }
+ } catch (error) {
+  return {
+   headers: {
+    'Access-Control-Allow-Origin': '*',
+   },
+   statusCode: 200,
+   body: 'Server Error',
+  }
+ }
+}
