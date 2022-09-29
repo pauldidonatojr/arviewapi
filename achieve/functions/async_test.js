c8fb66e7f24eb4e3db67893ac73ebf75ef09ccb2
Paul DiDonato <pjdidonato@galico.domainstacks.net>
2022-09-29 15:20:07 -0700

    push8

--- achieve/functions/async_test.js
+++
@@ -1,9 +0,0 @@
-// domain/.netlify/functions/async_test
-// exports
-
-exports.handler = async (event, context, cb) => {
- return {
-  statusCode: 200,
-  body: 'Async Test #1',
- }
-}
--- achieve/functions/synchronous.js
+++
@@ -1,6 +0,0 @@
-// domain/.netlify/functions/test
-// exports
-
-exports.handler = (event, context, cb) => {
- cb(null, { statusCode: 200, body: 'Test Server!' })
-}
